396e87917d31598321022f0b3d8738a2cece8d87
wbharath <wbharath51@gmail.com>
2025-10-12 18:45:45 -0300

    images added from Db

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -1,5 +1,12 @@
-function FavoriteToggleButton() {
-  return <div>Favorite Toggle</div>
+import { FaHeart } from 'react-icons/fa'
+import { Button } from '../ui/button'
+
+function FavoriteToggleButton({ productId }: { productId: string }) {
+  return (
+    <Button size="icon" variant="outline" className="p-2 cursor-pointer">
+      <FaHeart />
+    </Button>
+  )
 }
 
 export default FavoriteToggleButton
