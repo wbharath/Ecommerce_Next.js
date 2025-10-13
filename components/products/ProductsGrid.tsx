68c0b3ac762bfec0697e45a8b1ef16f0c7ae10ce
wbharath <wbharath51@gmail.com>
2025-10-13 17:59:34 -0300

    Products page list/grid view done

--- components/products/ProductsGrid.tsx
+++ components/products/ProductsGrid.tsx
@@ -3,6 +3,7 @@ import { Product } from '@prisma/client'
 import Link from 'next/link'
 import { Card, CardContent } from '../ui/card'
 import Image from 'next/image'
+import FavoriteToggleButton from './FavoriteToggleButton'
 function ProductsGrid({ products }: { products: Product[] }) {
   return (
     <div className="pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
@@ -25,9 +26,18 @@ function ProductsGrid({ products }: { products: Product[] }) {
                       className="rounded w-full object-cover transform group-hover:scale-110 transition-transform duration-500"
                     />
                   </div>
+                  <div className="mt-4 text-center">
+                    <h2 className="text-lg capitalize">{name}</h2>
+                    <p className="text-muted-foreground mt-2">
+                      {dollarsAmount}
+                    </p>
+                  </div>
                 </CardContent>
               </Card>
             </Link>
+            <div className="absolute top-7 right-7 z-5">
+              <FavoriteToggleButton productId={productId} />
+            </div>
           </article>
         )
       })}
