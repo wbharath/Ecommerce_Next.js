9dc2761523d0a6654dfeede212d3c694abec5956
wbharath <wbharath51@gmail.com>
2025-10-14 22:20:03 -0300

    Nav search implemented

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import Container from '../global/Container'
 import CartButton from './CartButton'
 import DarkMode from './DarkMode'
@@ -10,7 +11,9 @@ function Navbar() {
     <nav className="border-b">
       <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-8">
         <Logo />
-        <NavSearch />
+        <Suspense>
+          <NavSearch />
+        </Suspense>
         <div className="flex gap-4 items-center">
           <CartButton />
           <DarkMode />
