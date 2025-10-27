2276b2554873347b06c017d10b1af56ae458b140
wbharath <wbharath51@gmail.com>
2025-10-27 16:00:19 -0300

    params promise resolved

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -10,8 +10,13 @@ import SubmitReview from '@/components/reviews/SubmitReview'
 import ProductReview from '@/components/reviews/ProductReview'
 import { auth } from '@clerk/nextjs/server'
 
-async function SingleProductPage({ params }: { params: { id: string } }) {
-  const product = await fetchSingleProduct(params.id)
+async function SingleProductPage({
+  params
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const { id } = await params
+  const product = await fetchSingleProduct(id)
   const { name, image, company, description, price } = product
   const dollarsAmount = formatCurrency(price)
   const { userId } = auth()
@@ -37,22 +42,22 @@ async function SingleProductPage({ params }: { params: { id: string } }) {
           <div className="flex gap-x-8 items-center">
             <h1 className="capitalize text-3xl font-bold">{name}</h1>
             <div className="flex items-center gap-x-2">
-              <FavoriteToggleButton productId={params.id} />
+              <FavoriteToggleButton productId={id} />
               <ShareButton name={product.name} productId={product.id} />
             </div>
           </div>
-          <ProductRating productId={params.id} />
+          <ProductRating productId={id} />
           <h4 className="text-xl mt-2">{company}</h4>
           <p className="mt-3 text-md bg-muted inline-block p-2 rounded">
             {dollarsAmount}
           </p>
           <p className="mt-6 leading-8 text-muted-foreground">{description}</p>
-          <AddToCart productId={params.id} />
+          <AddToCart productId={id} />
         </div>
       </div>
       <br />
-      <ProductReview productId={params.id} />
-      {reviewDoesNotExist && <SubmitReview productId={params.id} />}
+      <ProductReview productId={id} />
+      {reviewDoesNotExist && <SubmitReview productId={id} />}
     </section>
   )
 }
