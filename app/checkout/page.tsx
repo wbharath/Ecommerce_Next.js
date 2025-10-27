bf9a3f243fe2c97d661b4e7281286a7f3142e940
wbharath <wbharath51@gmail.com>
2025-10-27 17:18:40 -0300

    Checkout content wrapped in suspense

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import axios from 'axios'
 import { useSearchParams } from 'next/navigation'
-import React, { useCallback } from 'react'
+import React, { useCallback, Suspense } from 'react'
 import { loadStripe } from '@stripe/stripe-js'
 import {
   EmbeddedCheckoutProvider,
@@ -12,22 +12,32 @@ const stripePromise = loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
 )
 
-function CheckoutPage() {
+function CheckoutContent() {
   const searchParams = useSearchParams()
   const orderId = searchParams.get('orderId')
   const cartId = searchParams.get('cartId')
+
   const fetchClientSecret = useCallback(async () => {
     const response = await axios.post('/api/payment', { orderId, cartId })
     return response.data.clientSecret
-  }, [])
+  }, [orderId, cartId]) // Added dependencies
 
-  const options = {fetchClientSecret}
+  const options = { fetchClientSecret }
+
   return (
     <div id="checkout">
       <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
         <EmbeddedCheckout />
       </EmbeddedCheckoutProvider>
     </div>
+  )
+}
+
+function CheckoutPage() {
+  return (
+    <Suspense fallback={<div>Loading checkout...</div>}>
+      <CheckoutContent />
+    </Suspense>
   )
 }
 
